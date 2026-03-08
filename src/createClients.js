d4ef3e6787a6cc7406c50ce1da55675b4ffcd347
izumi <naymyomg332@gmail.com>
2026-03-08 10:28:22 +0630

    pos-v21

--- src/createClients.js
+++ src/createClients.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 
-const supabase = createClient('https://bjstydtzxedjdxkunibl.supabase.co',
-    'sb_publishable_XiUFS9P5majdKa9Z2HN5Ig_y9Pe3aIa'
+const supabase = createClient("https://mjspwpyjujblmrdmytqz.supabase.co",
+    "sb_publishable_411Zs-n-VTdhxCIGjJ_wag_wP_nUyb_"
 );
 
 export default supabase;
