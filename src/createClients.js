28d06c7efe13ea0aa6eb0daeba30230142fc91df
izumi <naymyomg332@gmail.com>
2026-03-25 13:57:15 +0630

    test in Ai

--- src/createClients.js
+++ src/createClients.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Test Server 
-// https://gtvfahmaygedbdbzylxy.supabase.co
+// https://gtvfahmaygedbdbzylxy.supabase.c
 // sb_publishable_Z3rNkQLisZQe7kio7tzKfA_vaQKrc-z
 const supabase = createClient("https://gtvfahmaygedbdbzylxy.supabase.co",
     "sb_publishable_Z3rNkQLisZQe7kio7tzKfA_vaQKrc-z"
